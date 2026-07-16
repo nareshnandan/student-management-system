c4436cbb83c670dbef85ea82d25d61dd71819887
nareshnandan <nareshnandan23@gmail.com>
2026-07-16 06:50:12 +0530

    Initial project structure and class setup

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+
+class StudentManagement:
+
+    def __init__(self):
+        self.file_name = "students.json"
+
+    def load_data(self):
+        pass
+
+    def save_data(self):
+        pass
+
+    def add_student(self):
+        pass
+
+    def search_student(self):
+        pass
+
+    def update_student(self):
+        pass
+
+    def delete_student(self):
+        pass
+
+    def view_student(self):
+        pass
+
+    def view_all_students(self):
+        pass
+
+    def main_menu(self):
+        pass
+
+student = StudentManagement()
+student.main_menu()
